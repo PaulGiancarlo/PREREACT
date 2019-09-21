25acb194bd19a9784f295effb1133209cada1529
Paul Diaz <paulgiancarlod@gmail.com>
2019-09-21 20:46:42 -0300

    basic concept of Implicit type Coercion in js. Three test

--- ImplicitTypeCoercion.js
+++ ImplicitTypeCoercion.js
@@ -1,5 +1,9 @@
 function sum_itc(a,b){
     return a+b;
 }
-
+//Using Float number
 console.log(sum_itc(11/10,"9"));
+//using an Integer
+console.log(sum_itc(1,"2"));
+//using a float number representation
+console.log(sum_itc(1.1,"4"));
